2721ae3b7630013084e8fec16bdf8528835329b6
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-06 21:36:56 -0600

    chore(firebase-config): Updated build from lib to dist. Clean tsconfig.

--- todo-list-API/src/index.ts
+++ todo-list-API/src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-import { onRequest } from "firebase-functions/v2/https";
+import express, {Request, Response} from "express";
+import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 const app = express();
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  logger.info(`Hello, world!`, { structuredData: true });
-  res.send("Hello, world!");
+  logger.info("Hello, test!", {structuredData: true});
+  res.send("Hello, test!");
 });
 
 exports.api = onRequest(app);
