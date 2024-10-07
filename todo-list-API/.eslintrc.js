4b0cef5e743c9f347555fda54e160250b5093c92
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-06 21:48:51 -0600

    chore(fb-services): Installed dotenv and created firebase app initializer.

--- todo-list-API/.eslintrc.js
+++ todo-list-API/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["todo-list-API/tsconfig.json", "todo-list-API/tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
@@ -29,5 +29,8 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "require-jsdoc": 0, // Disable the JSDoc requirement
+    "valid-jsdoc": 0, // Disable validation of JSDoc comments
+    "new-cap": 0, // Disable the new-cap rule globally
   },
 };
