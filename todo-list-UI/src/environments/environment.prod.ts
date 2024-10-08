39fdb1e3adb180351950c9cc0992891a8bafbca0
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 11:36:07 -0600

    chore(dashboard): Card global style added. Task interface added. Environment values updated.

--- todo-list-UI/src/environments/environment.prod.ts
+++ todo-list-UI/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
 
   baseUrl: 'https://api-q2qsitwd6a-uc.a.run.app',
   api: "/api",
-	users: "/users"
+	users: "/users",
+	tasks: "/tasks",
 };
--- todo-list-UI/src/environments/environment.ts
+++ todo-list-UI/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   baseUrl: 'https://api-q2qsitwd6a-uc.a.run.app',
 	api: "/api",
-	users: "/users"
+	users: "/users",
+	tasks: "/tasks",
 };
