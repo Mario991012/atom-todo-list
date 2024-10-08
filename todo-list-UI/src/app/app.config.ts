2ba6820c6467c0d9a4204a0c5262ea9e321b246f
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 12:01:08 -0600

    chore(dashboard): Token interceptor to add token to Authorization header.

--- todo-list-UI/src/app/app.config.ts
+++ todo-list-UI/src/app/app.config.ts
@@ -1,10 +1,20 @@
-import { ApplicationConfig } from "@angular/core";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
-import { provideHttpClient } from "@angular/common/http";
+import { routes } from './app.routes';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+  ],
 };
