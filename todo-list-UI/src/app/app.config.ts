53bf009405ec896f2748fd05edaa365b58ee7ed0
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 12:29:39 -0600

    chore(dashboard): Firebase config added

--- todo-list-UI/src/app/app.config.ts
+++ todo-list-UI/src/app/app.config.ts
@@ -9,6 +9,8 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +18,16 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
     provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'atom-angular-todo',
+        appId: '1:993218519320:web:5bb14d94c60aeeefe6be6d',
+        storageBucket: 'atom-angular-todo.appspot.com',
+        apiKey: 'AIzaSyCCaW_Q8UDtSSLK2xsxCorVmyBYfhqdHUY',
+        authDomain: 'atom-angular-todo.firebaseapp.com',
+        messagingSenderId: '993218519320',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
 };
