90d577e7d5461b68cd2a5ef49ab1306757ccc5e1
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 17:29:13 -0600

    chore(firebase-hosting): Configure cors to allow new hosting URL to consume API.

--- todo-list-API/src/index.local.ts
+++ todo-list-API/src/index.local.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:4200"],
+  origin: ["http://localhost:4200", "https://atom-angular-todo.web.app"],
   credentials: true,
 }));
 
