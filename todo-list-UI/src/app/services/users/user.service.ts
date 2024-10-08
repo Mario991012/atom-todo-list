04f87cfde1bd3349e9eee89e2117e83d2895e475
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 16:34:36 -0600

    update(front-end-tests): Updated user service to use http service.

--- todo-list-UI/src/app/services/users/user.service.ts
+++ todo-list-UI/src/app/services/users/user.service.ts
@@ -1,22 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { CreateUserResponse, LoginUserResponse } from '../../shared/interfaces/user.interface';
+import { HttpService } from '../../core/services/http/http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
   private apiUrl = `${environment.baseUrl}${environment.api}${environment.users}`;
-
-  constructor(private http: HttpClient) {}
+  private httpService = inject(HttpService);
 
   login(email: string): Observable<LoginUserResponse> {
-    return this.http.get<LoginUserResponse>(`${this.apiUrl}/${email}`);
+    return this.httpService.get<LoginUserResponse>(`${this.apiUrl}/${email}`);
   }
 
   createUser(email: string): Observable<CreateUserResponse> {
-    return this.http.post<CreateUserResponse>(this.apiUrl, { email });
+    return this.httpService.post<CreateUserResponse>(this.apiUrl, { email });
   }
 }
