fca56f793012203146dc769f64cb7b55449f6500
Mario Velasquez <mvelasquezb99@gmail.com>
2024-10-08 15:11:52 -0600

    tests(front-unit-tests): Token services tests updated

--- todo-list-UI/src/app/core/services/token/token.service.spec.ts
+++ todo-list-UI/src/app/core/services/token/token.service.spec.ts
@@ -1,22 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TokenService } from './token.service';
+import { Auth } from '@angular/fire/auth';
+import { from, of, throwError } from 'rxjs';
+import { signInWithCustomToken } from '@angular/fire/auth';
 
 describe('TokenService', () => {
   let service: TokenService;
-  let httpMock: HttpTestingController;
+  let authMock: jasmine.SpyObj<Auth>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [TokenService],
-    });
-    service = TestBed.inject(TokenService);
-    httpMock = TestBed.inject(HttpTestingController);
-  });
+    const authSpy = jasmine.createSpyObj('Auth', ['']);
 
-  afterEach(() => {
-    httpMock.verify();
+    TestBed.configureTestingModule({
+      providers: [
+        TokenService,
+        { provide: Auth, useValue: authSpy },
+      ],
+    });
+
+    service = TestBed.inject(TokenService);
+    authMock = TestBed.inject(Auth) as jasmine.SpyObj<Auth>;
+
+    spyOn<any>(signInWithCustomToken, 'call').and.callFake(() => {
+      return from(Promise.resolve({
+        user: {
+          getIdToken: () => Promise.resolve('mockIdToken')
+        }
+      }));
+    });
   });
 
   it('should be created', () => {
@@ -50,6 +61,59 @@ describe('TokenService', () => {
     it('should return null if no token is found in localStorage', () => {
       spyOn(localStorage, 'getItem').and.returnValue(null);
       expect(service.getToken()).toBeNull();
+    });
+  });
+
+  describe('signInWithCustomToken', () => {
+    it('should call signInWithCustomToken and return userCredential', (done: DoneFn) => {
+      const customToken = 'customToken';
+      const userCredentialMock: any = {
+        user: {
+          getIdToken: jasmine.createSpy().and.returnValue(Promise.resolve('mockIdToken')),
+        },
+      };
+
+      spyOn(service, 'signInWithCustomToken').and.returnValue(of(userCredentialMock));
+
+      service.signInWithCustomToken(customToken).subscribe({
+        next: (userCredential) => {
+          expect(service.signInWithCustomToken).toHaveBeenCalledWith(customToken);
+          expect(userCredential).toEqual(userCredentialMock);
+          done();
+        },
+      });
+    });
+  });
+
+  describe('loginWithCustomToken', () => {
+    it('should store token after successful login', (done: DoneFn) => {
+      const customToken = 'customToken';
+      const mockToken = 'mockIdToken';
+      const userCredentialMock: any = {
+        user: {
+          getIdToken: jasmine.createSpy().and.returnValue(Promise.resolve(mockToken)),
+        },
+      };
+
+      spyOn(service, 'storeToken');
+      spyOn(service, 'signInWithCustomToken').and.returnValue(of(userCredentialMock));
+
+      service.loginWithCustomToken(customToken);
+
+      setTimeout(() => {
+        expect(userCredentialMock.user.getIdToken).toHaveBeenCalled();
+        expect(service.storeToken).toHaveBeenCalledWith(mockToken);
+        done();
+      }, 0);
+    });
+
+    it('should handle login errors', () => {
+      const customToken = 'customToken';
+      const consoleSpy = spyOn(console, 'error');
+      spyOn(service, 'signInWithCustomToken').and.returnValue(throwError(() => new Error('Login failed')));
+
+      service.loginWithCustomToken(customToken);
+      expect(consoleSpy).toHaveBeenCalledWith('Login failed', jasmine.any(Error));
     });
   });
 });
